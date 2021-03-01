34e1f29bf8e3502501f84afecbfc8dc9323ef7b3
angel <dewebisc@gmail.com>
2021-03-01 20:47:26 -0300

    component de api de marvel terminado

---
+++ src/app/home/components/api-section/api-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-api-section',
+  templateUrl: './api-section.component.html',
+  styleUrls: ['./api-section.component.css']
+})
+export class ApiSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { ApiSectionComponent } from './components/api-section/api-section.component';
 
 
 @NgModule({
-  declarations: [HomeComponent, BannerComponent],
+  declarations: [HomeComponent, BannerComponent, ApiSectionComponent],
     imports: [
         CommonModule,
         HomeRoutingModule
